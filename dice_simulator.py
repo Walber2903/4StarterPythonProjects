bc2bbdfcae50d4641c1e5ce609e3004438b90f97
walber2903 <walber.menezes@hotmail.com>
2022-12-17 18:47:43 -0300

    first project

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,19 +1,29 @@
 #Dice simulator
 #Simulate dice use, generate a value between 1 to 6.
 import random
+import PySimpleGUI as sg
 
 class DiceSimulator:
   def __init__(self):
     self.minumum_value = 1
     self.maximum_value = 6
     self.message = 'Do you like to roll a d6? (yes/no)'
+    # Layout
+    self.layout = [
+      [sg.Text('Roll the dice?')],
+      [sg.Button('Yes'), sg.Button('No')]
+    ]
 
   def Initiate(self):
-    response = input(self.message)
+    # Create a window
+    self.window = sg.Window('Dice Simulator', layout=self.layout)
+    # Ready the input values from screen
+    self.events, self.values = self.window.Read()
+    # Make something with the values
     try:
-      if response == 'yes' or response == 'y':
+      if self.events == 'Yes' or self.events == 'y':
         print(self.GenerateDiceValue())
-      elif response == 'no' or response == 'n':
+      elif self.events == 'No' or self.events == 'n':
         print('Thanks...Closing the program...')
       else:
         print('Please type yes or no.')
